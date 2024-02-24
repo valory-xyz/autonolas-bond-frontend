b46e8fccf98985a7a53cd08b44fbd8641d6de0cb
Oaksprout <66292936+oaksprout@users.noreply.github.com>
2024-02-24 16:44:10 -0700

    Refactor Hero component styling and add responsive behavior

--- components/Home/Hero.jsx
+++ components/Home/Hero.jsx
@@ -1,5 +1,5 @@
 import {
-  Card, Row, Col, Typography, Flex, Button,
+  Card, Row, Col, Typography, Flex, Button, Grid,
 } from 'antd';
 import { CloseOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
@@ -7,27 +7,26 @@ import Image from 'next/image';
 import { COLOR } from '@autonolas/frontend-library';
 import { useState } from 'react';
 
+const { useBreakpoint } = Grid;
+
 const StyledCard = styled(Card)`
   border-color: ${COLOR.BORDER_GREY};
   width: 100%;
-  margin-bottom: 24 px;
+  margin-bottom: 24px;
   .ant-card-body {
-    padding: 64px;
+    padding: ${(props) => (props.md ? '64px' : '24px')};
     flex-direction: column;
     align-items: center;
   }
 `;
 
-const ImageContainer = styled.div`
-    position: relative!;
-    width: 100%;
-`;
-
 export const Hero = () => {
   const [heroOpen, setHeroOpen] = useState(true);
+  const { md } = useBreakpoint();
+
   return (
     heroOpen && (
-    <StyledCard>
+    <StyledCard md={md}>
       <div // did not use styled component for this, css failed to apply
         style={{
           position: 'absolute', right: 10, top: 10, cursor: 'pointer',
@@ -39,23 +38,22 @@ export const Hero = () => {
         />
       </div>
 
-      <Row gutter={24}>
+      <Row gutter={64}>
         <Col md={12} xs={24}>
-          <ImageContainer>
-            <Image src="/images/bond.svg" fill />
-          </ImageContainer>
+          <Image src="/images/bond.svg" fill className="mx-auto" />
         </Col>
         <Col md={12} xs={24}>
-          <Flex vertical gap={8} style={{ height: '100%' }}>
-            <Typography.Title level={3} style={{ marginTop: 0 }}>Bond now</Typography.Title>
+          <Flex vertical gap={16} style={{ height: '100%' }}>
+            <Typography.Title level={3} className="mt-0 mb-0">
+              Pool. Bridge. Bond.
+            </Typography.Title>
             <Typography.Text>
-              Bonders provide tokens to the protocol, and receive discounted OLAS in exchange.
-            </Typography.Text>
-            <Typography.Text>
-              The bonding path is slightly different, depending on the token you deposit.
+              Pick a path and bond into the Olas protocol
+              {' '}
+              for the potential to receive discounted OLAS.
             </Typography.Text>
             <Flex style={{ justifySelf: 'end' }}>
-              <Button type="primary" onClick={() => setHeroOpen(false)}>Got it</Button>
+              <Button type="default" onClick={() => setHeroOpen(false)}>🫡 Got it</Button>
             </Flex>
           </Flex>
         </Col>
