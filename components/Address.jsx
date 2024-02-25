be16cd784796db4d8019384d440509a52a917333
Oaksprout <66292936+oaksprout@users.noreply.github.com>
2024-02-24 17:38:37 -0700

    Refactor Address component to remove unused AddressLink styled component

--- components/Address.jsx
+++ components/Address.jsx
@@ -9,12 +9,6 @@ const AddressText = styled(Typography.Text)`
   title: ${(props) => props.title};
 `;
 
-const AddressLink = styled.a`
-  href: ${(props) => props.href};
-  target: _blank;
-  rel: noreferrer noopener;
-`;
-
 const Address = ({ address, networkId }) => {
   const explorerBaseUrl = getExplorerBaseUrl(networkId);
 
@@ -23,11 +17,11 @@ const Address = ({ address, networkId }) => {
   return (
     <>
       <AddressText title={address}>
-        <AddressLink href={explorerUrl}>
+        <a href={explorerUrl} rel="noopener noreferrer" target="_blank">
           {truncateAddress(address)}
           {' '}
           ↗
-        </AddressLink>
+        </a>
       </AddressText>
     &nbsp;
       <Copy copyText={address} />
